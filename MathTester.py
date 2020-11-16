2da48d834ba279d3c4d50fb3cde08888ee23a835
Anthony Irwin <anthonyirwin@gmail.com>
2020-11-16 07:10:08 -0500

    Added Variable for Score

--- MathTester.py
+++ MathTester.py
@@ -3,15 +3,28 @@ import random
 # function that creates random numbers from 1 to 1000 and asks user math problem.
 
 def teststart():
-  testvara = int(random.randint(1, 1000))
-  testvaratwo = int(random.randint(1, 1000))
-  print("What's " + str(testvaratwo) + " plus " + str(testvara))
-  answer = input("Answer: ")
-  if answer == testvara + testvaratwo:
+
+  testvaraone = rannumone()
+  testvaratwo = rannumtwo()
+  score = 0
+  
+#  testvaratwo = int(random.randint(1, 1000))
+  print("What's " + str(testvaratwo) + " plus " + str(testvaraone))
+  
+  answer = int(input("Answer: "))
+  
+  if answer == testvaraone + testvaratwo:
     print("Good Job!")
+    score += 1
+    print("You have a score of " + str(score))
   else:
       print("How shameful, try again!!")
-  return
+  
+def rannumone():
+  return int(random.randint(1,1000))
+
+def rannumtwo():
+  return int(random.randint(1, 1000))
 
 # Start Menu where user is asked to put in their name and ask whether or not they want to take the math challenge.  
 namez = input(" Please enter your name:")
